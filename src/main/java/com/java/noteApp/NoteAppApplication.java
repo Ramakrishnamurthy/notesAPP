82b5abdca64b3ee30668cb38a83cf7d317be6410
BA-000698248 <ba-000698248@BA-000698248s-MacBook-Pro.local>
2025-02-17 17:54:21 +0530

    comment

--- src/main/java/com/java/noteApp/NoteAppApplication.java
+++ src/main/java/com/java/noteApp/NoteAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NoteAppApplication {
 
-	
+	//main method to start the Spring Boot application
 	public static void main(String[] args) {
 		SpringApplication.run(NoteAppApplication.class, args);
 	}
